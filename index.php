11bf12f519212c8d7ecc9ef2a03b0d066487f99e
Josh Miller <josh@joshnliz.com>
2013-03-12 10:14:30 -0400

    whoops... never clicked save

--- index.php
+++ index.php
@@ -36,12 +36,25 @@ if (!$db->query("SHOW TABLES LIKE 'extension_status'")->numrows) {
   $db->query("INSERT INTO  `commerce_slurp`.`extension_status` (`esid`) VALUES ('Recommended'), ('Active'), ('In-Development'), ('Not Recommended')");
 }
 
-// Initialize source types
+/* Initialize source types
+ * The source types regulate how the job_queue gets filled. Only source_types that
+ * should be refreshed can be refreshed. Additionally, the slots number gets
+ * decremented for every job run. Re-populating the job_queue with these kinds of jobs will only
+ * happen if the following conditions are met:
+ *   * slot number is zero
+ *   * the difference between today & last refresh is greater than
+ *   * the difference between today & strtotime(+[refresh_every] days, last_refresh)
+ */
 if (!$db->query("SHOW TABLES LIKE 'source_types'")->numrows) {
   $db->query(
    "CREATE TABLE  `commerce_slurp`.`source_types` (
     `stid` VARCHAR( 35 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
     `multi` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT  '1' COMMENT  'Whether this source is multi-valued',
+    `slots` INT NOT NULL COMMENT 'How many times can this job be executed until next refresh',
+    `slots_per_refresh` INT NOT NULL COMMENT 'Default number of slots',
+    `refresh_every` INT NOT NULL COMMENT '1 means this gets reset every day, 2 means this gets reset every 48 hours. Only source types with 0 slots can be refreshed.',
+    `stid` VARCHAR( 35 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'Relationship to source_type' ,
+    `last_refresh` TIMESTAMP NOT NULL ,
     PRIMARY KEY (  `stid` )
     ) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;");
   $db->query("INSERT INTO  `commerce_slurp`.`source_types` (`stid`,`multi`) VALUES ('Search Result',1), ('RSS',1), ('Module Page',0)");
@@ -57,27 +70,13 @@ if (!$db->query("SHOW TABLES LIKE 'primary_sources'")->numrows) {
     `stid` VARCHAR( 35 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'Relationship to source_type' ,
     `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
     `last_checked` TIMESTAMP NOT NULL ,
-    FOREIGN KEY ( `etid` ) REFERENCES extension_types(etid) ON DELETE CASCADE ,
-    FOREIGN KEY ( `stid` ) REFERENCES source_types(stid) ON DELETE CASCADE 
+    FOREIGN KEY ( `etid` ) REFERENCES extension_types(etid) ,
+    FOREIGN KEY ( `stid` ) REFERENCES source_types(stid) 
     ) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;");
   require_once 'primary_sources.php';
 }
 
 // Initialize extensions repository
-/*
-    "name" => $title,
-    "url" => str_replace("http://drupal.org/project/","",$url),
-    "author" => $author,
-    "status" => $status,
-    "downloads" => $downloads,
-    "installs" => $installs,
-    "bugs" => $open_bugs,
-    "created" => $created,
-    "modified" => $lastmodified,
-    "images" => $module_img,
-    "description" => $desc,
-    "module_pg_type" => $module_pg_type,
-*/
 if (!$db->query("SHOW TABLES LIKE 'extensions'")->numrows) {
   $db->query(
    "CREATE TABLE  `commerce_slurp`.`extensions` (
@@ -96,9 +95,38 @@ if (!$db->query("SHOW TABLES LIKE 'extensions'")->numrows) {
     `modified` TIMESTAMP NOT NULL ,
     `last_checked` TIMESTAMP NOT NULL ,
     FOREIGN KEY ( `esid` ) REFERENCES extension_status(esid) ,
-    FOREIGN KEY ( `etid` ) REFERENCES extension_types(etid) ON DELETE CASCADE ,
-    FOREIGN KEY ( `psid` ) REFERENCES primary_sources(psid) ON DELETE CASCADE 
+    FOREIGN KEY ( `etid` ) REFERENCES extension_types(etid) ,
+    FOREIGN KEY ( `psid` ) REFERENCES primary_sources(psid) 
     ) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;");
+}
+
+
+// Initialize job types
+if (!$db->query("SHOW TABLES LIKE 'jobs'")->numrows) {
+  $db->query(
+   "CREATE TABLE  `commerce_slurp`.`jobs` (
+    `jid` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `psid` INT UNSIGNED NOT NULL ,
+    `extension_id` INT UNSIGNED NOT NULL,
+    `stid` VARCHAR( 35 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'Relationship to source_type' ,
+    `last_refresh` TIMESTAMP NOT NULL ,
+    FOREIGN KEY ( `stid` ) REFERENCES source_types(stid)
+    ) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;");
+}
+
+/**
+ * Variables
+ */
+$slots_per_cron = 10; // Number of slots should be run per cron
+$now = time();
+$start_time = strtotime("8:00 am");
+$end_time = strtotime("5:00 pm");
+
+// Can only run during set period...
+if ($now >= $start || $now <= $end){
+  echo "Running cron...";
+} else {
+  echo "Not running cron because of period";
 }
 /**
  * Test pull in data
