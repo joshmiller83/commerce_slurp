f99071c65513fd8dc4e3797602720fb0c00dedaf
Josh Miller <josh@joshnliz.com>
2013-11-22 10:16:58 -0500

    Working for D7 now

--- functions.php
+++ functions.php
@@ -229,21 +229,36 @@ function commerce_slurp_add_job_from_source($job,$multi,$extension_id=NULL) {
 function commerce_slurp_page($extension) {
   global $db, $client;
   $crawler = $client->request('GET', $extension['url']);
-  $name = $crawler->filter("h1#page-subtitle");
-  if ($name->count() > 0) $data['name'] = addslashes($name->text());
+
+  // $data['name']
+  $name = $crawler->filter("h1");
+  if ($name->count() > 0) {
+    $data['name'] = addslashes($name->text());
+  }
+
+  // $data['author']
   $author = $crawler->filter(".node .submitted a");
-  if ($author->count() > 0) $data['author'] = $author->text();
+  if ($author->count() > 0) {
+    $data['author'] = $author->text();
+  } else {
+    $data['author'] = 'unknown';
+  }
+
+  // Metadata
   $data['status'] = "In-Development"; // see below, based on release version
   $data['downloads'] = 0;
   $data['installs'] = 0;
-  $data['bugs'] = $crawler->filter("div.issue-cockpit-bug div.issue-cockpit-totals a");
+  $data['bugs'] = $crawler->filter("div.issue-cockpit-1 div.issue-cockpit-totals a:first-child");
   $created = $crawler->filter("div.submitted em");
   if ($created->count() > 0) $data['created'] = $created->text();
   $data['last_modified'] = 0;
-  $images = $crawler->filter("div.field-field-project-images div.field-item a");
+  $images = $crawler->filter("div.field-name-field-project-images div.field-item a");
   if ($images->count() > 0) $data['images'] = $images->attr("href");
-  $desc = $crawler->filter("div.node-content");
-  if ($desc->count() > 0) $data['desc'] = $desc->text();
+
+  // Description
+  $desc = $crawler->filter("div.field-name-body .field-item");
+  if ($desc->count() > 0) $data['desc'] = mb_convert_encoding(utf8_decode($desc->html()), "HTML-ENTITIES", 'UTF-8');
+
   // Targeting on UL that has lots of data...
   // find maintenance & development status
   $maintenance_status = "";
@@ -285,6 +300,8 @@ function commerce_slurp_page($extension) {
             $title = implode(" ",$title);
             $data[$label] = $title;
           }
+        } else {
+          $data[$label] = $object;
         }
         break;
       case "status":
@@ -334,41 +351,7 @@ function commerce_slurp_page($extension) {
         $data[$label] = ($object->count() != 0) ? intval(str_ireplace(' open', '', $object->first()->text())) : 0;
         break;
       case "desc":
-        $desc = "";
-        // get paragraphs
-        $paragraphs = preg_split('/$\R?^/m', $object, -1, PREG_SPLIT_NO_EMPTY);
-        foreach ($paragraphs as $p) {
-
-          // go through each sentence, avoid if it's the disclaimer or headline, let it iterate to another paragraph
-          preg_match_all('/([^\.\?!]+[\.\?!])/', $p, $sentences);
-
-          if (count($sentences[0]) > 0) {
-            foreach ($sentences[0] as $sentence) {
-              // Avoid "obligatory" text and common header text
-              if (!stristr(@$sentence,"Recommended releases") &&
-                  !stristr(@$sentence,"This is a sandbox project") &&
-                  !stristr(@$sentence,"Git repository") &&
-                  strncmp(strtolower($sentence), "overview", 8) !== 0 &&
-                  strncmp(strtolower($sentence), "description", 11) !== 0 &&
-                  !stristr(@$sentence,"Experimental Project") &&
-                  strlen($desc) < 350){
-                $desc .= @$sentence . " ";
-              }
-              // Stop if we are at our maximum
-              if (strlen($desc) > 350) break;
-
-              // Stop if we have reached the download section
-              if (stristr(@$sentence,"Recommended releases") || trim(@$sentence)=="7.") break;
-            }
-          }
-          // Stop if we are at our maximum
-          if (strlen($desc) > 350) break;
-
-          // Stop if we have reached the download section
-          if (stristr(@$sentence,"Recommended releases") || trim(@$sentence)=="7.") break;
-        }
-
-        $data[$label] = addslashes(trim($desc));
+        $data[$label] = addslashes(trim($data[$label]));
         break;
       case "last_modified":
         // determine last commit (usually the most recent change is code to dev)
